06fc18b821bef820f759d53df7ad3417a6566416
Anton-Oxxom <anton.shchetynin@gmail.com>
2023-01-11 13:02:30 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
 
+    const bodyEl = document.querySelector('body');
+    const startBtn = document.querySelector('[data-start]');
+    const stopBtn = document.querySelector('[data-stop]');
+
+    let timer = null;
+
+startBtn.addEventListener("click", () => {
+    stopBtn.disabled = false;
+    timer = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    clearInterval(timer);
+    stopBtn.disabled = true;
+    
+});
