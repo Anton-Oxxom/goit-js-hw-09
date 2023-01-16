a7f3cc1f4404c21506ddb453e418cc96f9cd3099
Anton-Oxxom <anton.shchetynin@gmail.com>
2023-01-16 09:53:01 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delayPrm = Number(delayEl.value);
+
+  for (let i = 1; i <= amountEl.value; i += 1) {
+    createPromise(i, delayPrm).then(succes).catch(error);
+    delayPrm += Number(stepEl.value);
   }
+}
+
+
+
+function succes(result) {
+  Notiflix.Notify.success(result);
+}
+
+function error(error) {
+  Notiflix.Notify.failure(error);
 }
